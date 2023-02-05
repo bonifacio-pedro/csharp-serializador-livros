8e708674d2e4559fa930edb6f601c75b4e1fff79
Pedro Bonifácio <123605656+bonifacio-pedro@users.noreply.github.com>
2023-02-05 14:46:05 -0300

    Projeto adicionado

---
+++ Livros.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serializador_de_livros
+{
+    public class Livros
+    {
+        public string nome;
+        public string genero;
+        public double nota;
+        private static string caminho = @"save.xml";
+        public static void CriaXml(List<Livros> listaLivros)
+        {
+            try
+            {
+                XmlSerializer xmlEscritor = new XmlSerializer(typeof(List<Livros>));
+                
+                if (File.Exists(caminho))
+                {
+                    FileStream fs = File.Open(caminho, FileMode.Open);
+                    xmlEscritor.Serialize(fs, listaLivros);
+                    fs.Close();
+                }
+                else
+                {
+                    FileStream fs = File.Create(caminho);
+                    xmlEscritor.Serialize(fs, listaLivros);
+                    fs.Close();
+                }
+
+                Console.WriteLine("LIVROS SALVOS COM SUCESSO!");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("EXCEÇÃO: " + e.Message);
+            }
+            
+            
+        }
+
+        public static Livros CadastraLivro(ref Livros livro)
+        {
+            try
+            {
+                Console.Clear();
+                Console.Write("NOME DO LIVRO: ");
+                string nome = Console.ReadLine();
+                Console.Write("\r\nGÊNERO DO LIVRO: ");
+                string genero = Console.ReadLine();
+                Console.Write("\r\nNOTA DO LIVRO (0-10): ");
+                double nota = Convert.ToDouble(Console.ReadLine());
+
+                if (nota > 10 || nota < 0)
+                {
+                    throw new Exception("NOTA INVÁLIDA");
+                }
+
+                return livro = new Livros(nome.ToUpper(),genero.ToUpper(),nota);
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine("EXCEÇÃO: " + e.Message);
+                return null;
+            }
+            
+        }
+
+        public static List<Livros> EncheLista(ref List<Livros> listaAPreencher)
+        {
+            try
+            {
+                if (File.Exists(caminho))
+                {
+                    XmlSerializer xmlLeitor = new XmlSerializer(typeof(List<Livros>));
+                    FileStream fs = File.Open(caminho, FileMode.Open);
+                    List<Livros> listaLivros = (List<Livros>)xmlLeitor.Deserialize(fs);
+                    fs.Close();
+                    return listaAPreencher = listaLivros;
+                }
+                else
+                {
+                    return listaAPreencher = new List<Livros>();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("TIVEMOS UM ERRO EM CARREGAR O ARQUIVO PARA PREENCHER A LISTA: " + e.Message);
+                return null;
+            }
+            
+        }
+
+        public static void VerLivros()
+        {
+            try
+            {
+                XmlSerializer xmlLeitor = new XmlSerializer(typeof(List<Livros>));
+
+                if (File.Exists(caminho))
+                {
+                    FileStream fs = File.Open(caminho, FileMode.Open);
+                    List<Livros> listaLivros = (List<Livros>)xmlLeitor.Deserialize(fs);
+                    fs.Close();
+
+                    if (listaLivros.Count > 0)
+                    {
+                        foreach (Livros livro in listaLivros)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("==============================");
+                            Console.WriteLine("NOME: " + livro.nome);
+                            Console.WriteLine("GÊNERO: " + livro.genero);
+                            Console.WriteLine("NOTA: " + livro.nota);
+                            Console.WriteLine("==============================");
+
+                            Console.WriteLine(listaLivros.IndexOf(livro) + "-" + (Convert.ToUInt32(listaLivros.Count) - 1));
+                            Console.ReadKey();
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("AINDA NÃO TEMOS NENHUM LIVRO CADASTRADO.");
+                    }
+                }
+                else
+                {
+                    throw new Exception("AINDA NÃO TEMOS NENHUM ARQUIVO DE LIVROS SALVO.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("EXCEÇÃO: " + e.Message);
+                Console.ReadKey();
+            }
+            
+        }
+        public Livros(string nome, string genero, double nota)
+        {
+            this.nome = nome;
+            this.genero = genero;
+            this.nota = nota;
+        }
+
+        public Livros()
+        {
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+namespace Serializador_de_livros
+{
+    internal class Program
+    {
+        public static void Escolha(ref string escolha)
+        {
+            Console.Clear();
+            Console.WriteLine("(1) CADASTRAR LIVRO");
+            Console.WriteLine("(2) VER LIVROS");
+            Console.WriteLine("(3) SALVAR");
+            Console.WriteLine("(4) SAIR");
+
+            try
+            {
+                string escolhaTemp = Console.ReadKey(true).KeyChar.ToString().ToLower();
+                escolha = escolhaTemp;
+            } catch(Exception e)
+            {
+                Console.WriteLine("EXCEÇÃO: " + e.Message);
+                Console.ReadKey();
+            }
+        }
+
+        private static void Mensagem(string mensagem, bool clear)
+        {
+            if (clear)
+            {
+                Console.Clear();
+                Console.WriteLine(mensagem);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine(mensagem);
+                Console.ReadKey();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string escolha = "";
+            List<Livros> todosLivros = null;
+            Livros.EncheLista(ref todosLivros);
+
+            do
+            {
+                Escolha(ref escolha);
+
+                if(escolha == "1")
+                {
+                    Livros livro = null;
+                    
+
+                    if (Livros.CadastraLivro(ref livro) != null)
+                    {
+                        todosLivros.Add(livro);
+                        Mensagem("LIVRO CADASTRADO COM SUCESSO!", true);
+                    }
+                }
+                else if (escolha == "2")
+                {
+                    Livros.VerLivros();
+                }
+                else if (escolha == "3")
+                {
+                    Console.WriteLine("DESEJA SALVAR SEUS LIVROS? (S/N)");
+
+                    string escolhaTemp = Console.ReadKey(true).KeyChar.ToString().ToLower();
+
+                    if (escolhaTemp == "s")
+                    {
+                        Livros.CriaXml(todosLivros);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                }
+                else if (escolha == "4")
+                {
+                    Mensagem("SAINDO....", true);
+                    break;
+                }
+                else
+                {
+                    Mensagem("FAÇA UMA ESCOLHA VÁLIDA!", true);
+                    continue;
+                }
+
+            } while (escolha != "4");
+        }
+    }
+}
